8ab848f2784d05c38e42f340c5f0b5c4883e2897
ikariuki-crypto <i.kariuki@alustudent.com>
2026-07-31 18:25:37 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains BaseGeometry class with area method
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an Exception indicating area is not implemented"""
+        raise Exception("area() is not implemented")
